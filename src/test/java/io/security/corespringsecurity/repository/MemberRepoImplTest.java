d3a9639c29a0121f0f9abd27cea605f0e35f3cd0
xi_jjun <rlawowns97@naver.com>
2022-03-09 16:22:54 +0900

    test: test the MemberRepository for checking creation

--- src/test/java/io/security/corespringsecurity/repository/MemberRepoImplTest.java
+++ src/test/java/io/security/corespringsecurity/repository/MemberRepoImplTest.java
@@ -3,35 +3,39 @@ package io.security.corespringsecurity.repository;
 import static org.junit.jupiter.api.Assertions.*;
 
 import io.security.corespringsecurity.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
+@SpringBootTest
 @Transactional
 class MemberRepoImplTest {
 	@Autowired
 	private MemberRepository memberRepository;
-	@Autowired
-	private EntityManager em;
+
+	@BeforeEach
+	void makeDummy() {
+		Member memberA = createMember("A", "AAA123", "1234", "AaaA", 26);
+		Member memberB = createMember("B", "BBB123", "1234", "BbbB", 24);
+		Member memberC = createMember("C", "CCC1234", "1234", "CccC", 20);
+		memberRepository.save(memberA);
+		memberRepository.save(memberB);
+		memberRepository.save(memberC);
+	}
 
 	@Test
 	@DisplayName("repo save test")
 	void save() {
 		// given
-		Member toBeSaved = new Member();
-		toBeSaved.setName("김재준");
-		toBeSaved.setAge(26);
-		toBeSaved.setNickname("공돌이");
-		toBeSaved.setId("rlawowns1234");
-		toBeSaved.setPassword("12345");
+		Member toBeSaved = createMember("김재준", "rlawowns1234", "1234", "GongDoll", 26);
 
 		// when
-		Member save = memberRepository.save(toBeSaved);
+		memberRepository.save(toBeSaved);
 
 		// then
 		List<Member> members = memberRepository.findAll();
@@ -45,5 +49,33 @@ class MemberRepoImplTest {
 		assertEquals(toBeSaved.getAge(), findMember.getAge());
 		assertEquals(toBeSaved.getName(), findMember.getName());
 		assertEquals(toBeSaved.getNickname(), findMember.getNickname());
+	}
+
+	private Member createMember(String name, String id, String password, String nickname, int age) {
+		Member toBeSaved = new Member();
+		toBeSaved.setName(name);
+		toBeSaved.setNickname(nickname);
+		toBeSaved.setId(id);
+		toBeSaved.setPassword(password);
+		toBeSaved.setAge(age);
+		return toBeSaved;
+	}
+
+	@Test
+	@DisplayName("findAll Test")
+	void findAll() {
+		// given
+		Member member = createMember("FINAL_MEMBER", "fi", "123", "f-ial", 16);
+		memberRepository.save(member);
+
+		// when
+		List<Member> members = memberRepository.findAll();
+		Member findOne = memberRepository.findOne((long) members.size());
+
+		// then
+		assertEquals(member, findOne); // idx check
+		for (Member mem : members) {
+			System.out.println("mem = " + mem);
+		}
 	}
 }
