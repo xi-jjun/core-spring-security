739deeca07d83a14bd53725f8126b06171130df0
xi_jjun <rlawowns97@naver.com>
2022-03-09 19:30:46 +0900

    feat: 지난시간에 만들었던 CustomMemberDetailsService 구현체의 반환값으로 추가적인 검증을 진행하는 CustomAuthenticationProvider class 를 구현

---
+++ src/main/java/io/security/corespringsecurity/security/provider/CustomAuthenticationProvider.java
@@ -0,0 +1,72 @@
+package io.security.corespringsecurity.security.provider;
+
+import io.security.corespringsecurity.security.service.MemberContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * CustomMemberDetailsService 구현체가 반환하는 UserDetails 객체를 받아,
+ * 추가적인 검증을 진행하는 class 인 CustomAuthenticationProvider 를 구현.
+ */
+// @RequiredArgsConstructor // SecurityConfig 에서 Bean 으로 등록 위해 기본 생성자 필요.
+// 따라서 아래 field 를 @Autowired 로 받았음.
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+	@Autowired
+	// 우리가 지난 시간에 만들었던 CustomMemberDetailsService 구현체 사용하기 위함.
+	private UserDetailsService userDetailsService;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	/**
+	 * 검증을 위한 구현 Method
+	 * @param authentication : authenticationManager 클래스로부터 전달 받는 인증객체.
+	 *                       인증할 때 사용자가 입력한 ID, Password 정보가 있다.
+	 */
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String username = authentication.getName();
+		String password = (String) authentication.getCredentials(); // 사용자가 입력한 password
+
+		// ID 검증
+		MemberContext memberContext = (MemberContext) userDetailsService.loadUserByUsername(username);
+
+		/**
+		 * Password 검증 => ID 가 있다는 것이 위에서 확인됨. 따라서 비번을 검증할 차례
+		 * 사용자가 입력한 password 와 MemberContext 의 password(DB 에 저장된 암호화된 비번) 이 동일해야 한다.
+		 */
+		if (!passwordEncoder.matches(password, memberContext.getMember().getPassword())) {
+			throw new BadCredentialsException("Bad Credentials Error");
+		}
+
+		/**
+		 * 최종적으로 인증에 성공한 인증 객체를 생성.
+		 */
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+				memberContext.getMember(),
+				null,
+				memberContext.getAuthorities() // memberContext 는 User 상속받았기에 해당 method 사용가능
+				// 권한은 CustomMemberDetailsService 구현체에서  MemberContext 생성할 때 roles List 객체로 전달했었음 이미.
+		);
+
+		return authenticationToken;
+	}
+
+	/**
+	 * param 의 type 과 현재 클래스가 사용하고자 하는 token 의 타입이 일치할 때
+	 * 이 provider 가 인증처리를 할 수 있게 하기 위한 method
+	 *
+	 * @param authentication
+	 * @return
+	 */
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+	}
+}
