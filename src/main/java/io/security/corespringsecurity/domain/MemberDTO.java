fec0accb65b1cdf80be458999570c953e63d7f54
xi_jjun <rlawowns97@naver.com>
2022-03-09 16:12:43 +0900

    comment: add description of html name and field name for remembering how to mapping

--- src/main/java/io/security/corespringsecurity/domain/MemberDTO.java
+++ src/main/java/io/security/corespringsecurity/domain/MemberDTO.java
@@ -4,6 +4,9 @@ import lombok.Data;
 
 @Data
 public class MemberDTO {
+	/**
+	 * html 의 name attribute 랑 field name 이랑 같아야 매핑이 된다.
+	 */
 	private String name;
 	private String id;
 	private String password;
--- src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
@@ -18,33 +18,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	/**
 	 * 사용자 만들어서 각각 권한 부여하는 method
+	 * 메모리 방식으로 인증처리하는 방법. => DB 로 할거니깐 주석처리함.
 	 */
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		String password = passwordEncoder().encode("1111");
-//		String password = "1111"; // 이 코드는 오류가 난다.
-
-		/**
-		 * "user" 라는 사용자의 비번은 password 이고, 권한은 "USER" 인 사용자를 생성하겠다.
-		 * 현재 3명의 사용자 생성. 각각 다른 권한을 부여함.
-		 *
-		 * password 는 암호화된 방식으로 줘야 오류를 안뱉는다.
-		 */
-		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
-		// admin 이라는 사용자에게 ADMIN, USER, MANAGER 라는 권한을 부여함. 따라서 3가지에 해당하는 권한을 쓸 수 있는 것이다.
-		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "USER", "MANAGER");
-//		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
-		/**
-		 * ADMIN 이 더 높은지 컴퓨터는 모른다
-		 */
-
-		/**
-		 * 권한이 없는 url 에 접근하면 403 error
-		 * 403 ERROR (wiki) : HTTP 403, 403 Forbidden 은 서버가 허용하지 않는 웹 페이지나 미디어를 사용자가 요청할 때
-		 * 웹 서버가 반환하는 HTTP 상태 코드
-		 */
-	}
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		String password = passwordEncoder().encode("1111");
+////		String password = "1111"; // 이 코드는 오류가 난다.
+//
+//		/**
+//		 * "user" 라는 사용자의 비번은 password 이고, 권한은 "USER" 인 사용자를 생성하겠다.
+//		 * 현재 3명의 사용자 생성. 각각 다른 권한을 부여함.
+//		 *
+//		 * password 는 암호화된 방식으로 줘야 오류를 안뱉는다.
+//		 */
+//		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
+//		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+//		// admin 이라는 사용자에게 ADMIN, USER, MANAGER 라는 권한을 부여함. 따라서 3가지에 해당하는 권한을 쓸 수 있는 것이다.
+//		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "USER", "MANAGER");
+////		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+//		/**
+//		 * ADMIN 이 더 높은지 컴퓨터는 모른다
+//		 */
+//
+//		/**
+//		 * 권한이 없는 url 에 접근하면 403 error
+//		 * 403 ERROR (wiki) : HTTP 403, 403 Forbidden 은 서버가 허용하지 않는 웹 페이지나 미디어를 사용자가 요청할 때
+//		 * 웹 서버가 반환하는 HTTP 상태 코드
+//		 */
+//	}
 
 	/**
 	 * literal 하게 선언된 문자열이 암호화가 된다.
@@ -73,7 +74,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeHttpRequests()
-				.antMatchers("/").permitAll() // 보안 필터의 검사를 받는다. 단지 그 결과가 모든 접근에 대한 허락일 뿐이다.
+				.antMatchers("/", "member/login/**", "/members").permitAll() // 보안 필터의 검사를 받는다. 단지 그 결과가 모든 접근에 대한 허락일 뿐이다.
 				// 그러나 ignoring 은 필터 자체를 거치지 않는 것이다.
 				.antMatchers("/my-page").hasRole("USER") // "/my-page" 경로에 USER 라는 권한을 가졌다면 접근 가능하게 한다.
 				.antMatchers("/manager").hasRole("MANAGER")
