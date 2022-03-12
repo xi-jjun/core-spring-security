980579eb31f6e646b28ef29fdb3c669ca2ddd7f1
xi_jjun <rlawowns97@naver.com>
2022-03-12 18:01:13 +0900

    feat: 인증을 성공한 후에 접근권한이 없는 자원에 접근하려고 할 때 예외처리를 해주는 accessDeniedHandler를 구현

---
+++ src/main/java/io/security/corespringsecurity/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,27 @@
+package io.security.corespringsecurity.security.handler;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 인증을 성공한 이후에!!
+ * 권한이 없는 자원에 접근하려고 할 때 AccessDeniedHandler 가 처리해준다.
+ */
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+	private String errorPage;
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+		String deniedUrl = errorPage + "?exception=" + accessDeniedException.getMessage();
+		response.sendRedirect(deniedUrl);
+	}
+
+	public void setErrorPage(String errorPage) {
+		this.errorPage = errorPage;
+	}
+}
